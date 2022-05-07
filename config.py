bf1bfff01687f6d0ee4bccfa0b0d8a0745e54e31
Seidazymov Adil <m.kypshakb@gmail.com>
2022-05-07 16:04:08 +0300

    - Now the value POSTS_PER_PAGE equals to 5 instead of 25

--- config.py
+++ config.py
@@ -9,6 +9,6 @@ class Config(object):
     SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL') or 'sqlite:///' + os.path.join(basedir, 'app.db')
     SQLALCHEMY_TRACK_MODIFICATIONS = False
 
-    POSTS_PER_PAGE = 25
+    POSTS_PER_PAGE = 5
 
     LANGUAGES = ['en', 'ru', "kk"]
