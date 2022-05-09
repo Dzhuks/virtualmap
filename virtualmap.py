ac8af23c4c9e345d9487da927c3e2b5331e3645a
Seidazymov Adil <m.kypshakb@gmail.com>
2022-05-09 21:29:18 +0300

    - Heroku

--- virtualmap.py
+++ virtualmap.py
@@ -1,5 +1,6 @@
 from app import create_app, db, cli
 from app.models import User, Post
+import os
 
 app = create_app()
 cli.register(app)
@@ -8,3 +9,8 @@ cli.register(app)
 @app.shell_context_processor
 def make_shell_context():
     return {'db': db, 'User': User, 'Post': Post}
+
+
+if __name__ == '__main__':
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host='0.0.0.0', port=port)
