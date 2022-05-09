d205035d4cb8eab3b83e1c395f67969e38df5761
NonQuant <husainov.21.04@gmail.com>
2022-05-09 17:48:32 +0500

    Исправление ошибки с регистрацией и входом

--- app/auth/forms.py
+++ app/auth/forms.py
@@ -7,27 +7,27 @@ from flask_babel import _, lazy_gettext as _l
 
 
 class LoginForm(FlaskForm):
-    username = StringField(_l('Пайдаланушы аты'), validators=[DataRequired()])
-    password = PasswordField(_l('Құпиясөз'), validators=[DataRequired()])
-    remember_me = BooleanField(_l('Есте сақтау'))
-    submit = SubmitField(_l('Кіру'))
+    username = StringField(_l('Username'), validators=[DataRequired()])
+    password = PasswordField(_l('Password'), validators=[DataRequired()])
+    remember_me = BooleanField(_l('Remember Me'))
+    submit = SubmitField(_l('Sign In'))
 
 
 class RegistrationForm(FlaskForm):
-    username = StringField(_l('Пайдаланушы аты'), validators=[DataRequired()])
+    username = StringField(_l('Username'), validators=[DataRequired()])
     email = StringField(_l('Email'), validators=[DataRequired(), Email()])
-    password = PasswordField(_l('Құпиясөз'), validators=[DataRequired()])
+    password = PasswordField(_l('Password'), validators=[DataRequired()])
     password2 = PasswordField(
-        _l('Құпиясөзді қайта теріңіз'), validators=[DataRequired(),
-                                           EqualTo('Құпиясөз')])
-    submit = SubmitField(_l('Тіркелу'))
+        _l('Repeat Password'), validators=[DataRequired(),
+                                           EqualTo('password')])
+    submit = SubmitField(_l('Register'))
 
     def validate_username(self, username):
         user = db.session.query(User).filter(User.username == username.data).first()
         if user is not None:
-            raise ValidationError(_('Басқа пайдаланушы атын таңдаңыз'))
+            raise ValidationError(_('Please use a different username.'))
 
     def validate_email(self, email):
         user = db.session.query(User).filter(User.email == email.data).first()
         if user is not None:
-            raise ValidationError(_('Басқа email таңдаңыз'))
+            raise ValidationError(_('Please use a different email address.'))
